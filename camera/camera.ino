fa37b9d98cb08822db0d71a96a95613cc7149755
wharris23 <147620144+wharris23@users.noreply.github.com>
2025-11-30 13:22:18 -0600

    Hide Wi-Fi credentials in camera.ino
    Updated Wi-Fi credentials to hide sensitive information.

--- camera/camera.ino
+++ camera/camera.ino
@@ -2,8 +2,8 @@
 #include <WiFi.h>
 
 // === Wi-Fi credentials ===
-const char* ssid = "ATTfEqa6if";
-const char* password = "hb6axzafb8bn";
+const char* ssid = "<hidden>";
+const char* password = "<hidden>";
 
 // === Freenove ESP32-WROVER OV3660 Pin Mapping ===
 #define PWDN_GPIO_NUM    -1
